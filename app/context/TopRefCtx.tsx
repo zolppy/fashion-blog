f7bbeba6f16f86a2f260e3df7f314eb21b0e4a73
Gabriel Cavalcante <gabriel.cavalcant.pb@compasso.com.br>
2024-12-02 18:31:08 -0300

    fix: resolvido problemas de build

--- app/context/TopRefCtx.tsx
+++ app/context/TopRefCtx.tsx
@@ -4,7 +4,6 @@ import {
     createContext,
     MutableRefObject,
     ReactNode,
-    RefObject,
     useContext,
     useRef,
 } from "react";
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { RefObject, useRef, useState } from "react";
+import { useState } from "react";
 import Image from "next/image";
 import {
     BsFacebook,
